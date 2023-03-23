5cac062fb06c9738d7e309447019ff16923bd46a
thaopx <thaopx@vnpt.vn>
2023-03-23 17:52:09 +0700

    dialog login

--- lib/screens/onboding/onboding_screen.dart
+++ lib/screens/onboding/onboding_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
+import 'package:animated_app_with_rive/screens/components/animated_btn.dart';
+import 'package:animated_app_with_rive/screens/components/custom_sign_in_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -8,8 +13,104 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  late RiveAnimationController _btnAnimationColtroller; // chuyển động của button
+  bool isSignInDialogShown = false; // hiện dialog đăng nhập
+  @override
+  void initState() {
+    _btnAnimationColtroller = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: BackdropFilter(
+              // Now it's looks perfect
+              // See how easy
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+            ),
+          ),
+          const RiveAnimation.asset("assets/RiveAssets/shapes.riv"),
+          Positioned.fill(
+            child: BackdropFilter(
+              // Làm mờ video bên dưới
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          AnimatedPositioned(
+            top: isSignInDialogShown ? -50 : 0, // đẩy background login lên trên sau khi hiện dialog login
+            duration: const Duration(milliseconds: 240),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    SizedBox(
+                      width: 260,
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Learn design & code",
+                            style: TextStyle(
+                              fontSize: 60,
+                              fontFamily: "Poppins",
+                              height: 1.2,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            "Don’t skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools.",
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(flex: 2),
+                    AnimatedBtn(
+                      btnAnimationColtroller: _btnAnimationColtroller,
+                      press: () {
+                        _btnAnimationColtroller.isActive = true;
+                        Future.delayed(
+                            const Duration(milliseconds: 800),
+                            () {
+                              setState(() {
+                                isSignInDialogShown = true; // chuyển trạng thái
+                              });
+                              customSigninDialog(
+                                context,
+                                onCLosed: (_) {
+                                  setState(() {
+                                    isSignInDialogShown = false; // chuyển trạng thái
+                                  });
+                                },
+                              );
+                            },
+                          );
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                        "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
