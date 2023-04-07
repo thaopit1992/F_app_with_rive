cefb2169374160cd3507164f52f80575687b6b41
thaopx <thaopx@vnpt.vn>
2023-04-07 14:59:10 +0700

    finish

--- lib/models/rive_asset.dart
+++ lib/models/rive_asset.dart
@@ -16,8 +16,12 @@ class RiveAsset {
 }
 
 List<RiveAsset> bottomNavs = [
-  RiveAsset("assets/RiveAssets/icons.riv",
-      artboard: "CHAT", stateMachineName: "CHAT_Interactivity", title: "Chat"),
+  RiveAsset(
+    "assets/RiveAssets/icons.riv",
+    artboard: "HOME",
+    stateMachineName: "HOME_interactivity",
+    title: "Home",
+  ),
   RiveAsset("assets/RiveAssets/icons.riv",
       artboard: "SEARCH",
       stateMachineName: "SEARCH_Interactivity",
@@ -34,4 +38,46 @@ List<RiveAsset> bottomNavs = [
       artboard: "USER",
       stateMachineName: "USER_Interactivity",
       title: "Profile"),
+];
+
+List<RiveAsset> sideMenus = [
+  RiveAsset(
+    "assets/RiveAssets/icons.riv",
+    artboard: "HOME",
+    stateMachineName: "HOME_interactivity",
+    title: "Home",
+  ),
+  RiveAsset(
+    "assets/RiveAssets/icons.riv",
+    artboard: "SEARCH",
+    stateMachineName: "SEARCH_Interactivity",
+    title: "Search",
+  ),
+  RiveAsset(
+    "assets/RiveAssets/icons.riv",
+    artboard: "LIKE/STAR",
+    stateMachineName: "STAR_Interactivity",
+    title: "Favorites",
+  ),
+  RiveAsset(
+    "assets/RiveAssets/icons.riv",
+    artboard: "CHAT",
+    stateMachineName: "CHAT_Interactivity",
+    title: "Help",
+  ),
+];
+
+List<RiveAsset> sideMenu2 = [
+  RiveAsset(
+    "assets/RiveAssets/icons.riv",
+    artboard: "TIMER",
+    stateMachineName: "TIMER_Interactivity",
+    title: "History",
+  ),
+  RiveAsset(
+    "assets/RiveAssets/icons.riv",
+    artboard: "BELL",
+    stateMachineName: "BELL_Interactivity",
+    title: "Notification",
+  ),
 ];
